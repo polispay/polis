81d40c5d5a2f4139e85517b35c18a79748d8ee7c
Cronos <cronos@polispay.org>
2019-01-20 20:29:30 -0600

    Preparing SPORK activation on MNPayments and NET level

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1390,15 +1390,33 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
             return false;
         }
 
-        if (nVersion < MIN_PEER_PROTO_VERSION)
+        if (sporkManager.IsSporkActive(SPORK_10_MASTERNODE_PAY_UPDATED_NODES))
         {
-            // disconnect from peers older than this proto version
-            LogPrintf("peer=%d using obsolete version %i; disconnecting\n", pfrom->id, nVersion);
-            connman.PushMessage(pfrom, CNetMsgMaker(INIT_PROTO_VERSION).Make(NetMsgType::REJECT, strCommand, REJECT_OBSOLETE,
-                               strprintf("Version must be %d or greater", MIN_PEER_PROTO_VERSION)));
-            pfrom->fDisconnect = true;
-            return false;
+            if (nVersion < MIN_PEER_PROTO_VERSION) {
+                // disconnect from peers older than this proto version
+                LogPrintf("peer=%d using obsolete version %i; disconnecting\n", pfrom->id, nVersion);
+                connman.PushMessage(pfrom, CNetMsgMaker(INIT_PROTO_VERSION).Make(NetMsgType::REJECT, strCommand,
+                                                                                 REJECT_OBSOLETE,
+                                                                                 strprintf(
+                                                                                         "Version must be %d or greater",
+                                                                                         MIN_PEER_PROTO_VERSION)));
+                pfrom->fDisconnect = true;
+                return false;
+            }
+        } else {
+            if (nVersion < PRESPORK_PROTOCOL) {
+                // disconnect from peers older than this proto version
+                LogPrintf("peer=%d using obsolete version %i; disconnecting\n", pfrom->id, nVersion);
+                connman.PushMessage(pfrom, CNetMsgMaker(INIT_PROTO_VERSION).Make(NetMsgType::REJECT, strCommand,
+                                                                                 REJECT_OBSOLETE,
+                                                                                 strprintf(
+                                                                                         "Version must be %d or greater",
+                                                                                         MIN_PEER_PROTO_VERSION)));
+                pfrom->fDisconnect = true;
+                return false;
+            }
         }
+
 
         if (nVersion == 10300)
             nVersion = 300;
--- src/version.h
+++ src/version.h
@@ -22,6 +22,9 @@ static const int GETHEADERS_VERSION = 70077;
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = 70212;
 
+//! disconnect from peers older than this proto version
+static const int PRESPORK_PROTOCOL = 70211;
+
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
 static const int CADDR_TIME_VERSION = 31402;
