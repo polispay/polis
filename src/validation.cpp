4466fc097bfa5529d2fa3926a7028675a61da260
Cronos <eabz@polispay.org>
2019-09-09 11:31:01 -0500

    Fixes

--- src/validation.cpp
+++ src/validation.cpp
@@ -110,7 +110,7 @@ static void CheckBlockIndex(const Consensus::Params& consensusParams);
 /** Constant stuff for coinbase transactions we create: */
 CScript COINBASE_FLAGS;
 
-const std::string strMessageMagic = "DarkCoin Signed Message:\n";
+const std::string strMessageMagic = "Polis Signed Message\n";
 
 // Internal stuff
 namespace {
